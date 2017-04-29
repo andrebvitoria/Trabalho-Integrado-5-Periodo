939a6d130e971fa6bf2ebb0daa6e9d84c58237f0
andrebvitoria <andre_davitoria@hotmail.com>
2017-04-28 23:21:06 -0300

    Update models.py

--- AltusCaldus/mysite/servicos/models.py
+++ AltusCaldus/mysite/servicos/models.py
@@ -2,26 +2,76 @@ from django.db import models
 from django.utils import timezone
 
 
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200, null=False)
+    cpf = models.CharField(max_length=11, null=False)
+    email = models.EmailField()
+    GENERO = ('Masculino', 'Masculino'), ('Feminino', 'Feminino')
+    genero = models.CharField(max_length=9, choices=GENERO)
+    celular = models.IntegerField()
+    telefone = models.IntegerField()
+    emergencia = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nome
+
+
+class Aluno(Pessoa):
+    def __str__(self):
+        return self.nome
+
+
+class Professor(Pessoa):
+    def __str__(self):
+        return self.nome
+
+
+class Servico(models.Model):
+    data = models.DateField()
+    valor = models.FloatField()
+    desconto = models.FloatField()
+    cliente = models.ForeignKey(Aluno)
+    vendedor = models.ForeignKey('auth.User')
+
+    def prestar_servico(self, pagamento):
+        troco = pagamento - self.valor + self.desconto
+        return troco
+
+    class Meta:
+        abstract = True
+
 
 class Item(models.Model):
-    item = models.CharField(max_length=100, null=False)
+    nome = models.CharField(max_length=100, null=False)
     descricao = models.CharField(max_length=200, null=False)
-    data_entrada = models.DateTimeField(default=timezone.now())
+    data_entrada = models.DateField()
 
     def __str__(self):
-        return self.item
+        return self.nome
 
-class Guarderia(models.Model):
-    cliente = models.CharField(max_length=100, null=False)
+
+class Guarderia(Servico):
     item = models.ForeignKey(Item, blank=True, null=False)
+    vencimento = models.DateField()
+
+    def prestar_servico(self, pagamento):
+        troco = pagamento - self.valor + self.desconto
+        if troco > -1:
+            self.vencimento = models.DateField(default=timezone.now() + timezone.timedelta(days=30))
+        return troco
+
+    def renovar(self, pagamento):
+        return self.prestar_servico(self, pagamento)
 
     def __str__(self):
-        return self.cliente
+        return self.cliente.nome
 
 
-class Tipo_Prancha(models.Model):
+class TipoPrancha(models.Model):
     descricao = models.CharField(max_length=200, null=False)
-
 
     def __str__(self):
         return self.descricao
@@ -29,18 +79,44 @@ class Tipo_Prancha(models.Model):
 
 class Prancha(models.Model):
     descricao = models.CharField(max_length=200, null=False)
-    tipo_prancha = models.ForeignKey(Tipo_Prancha)
+    tipo_prancha = models.ForeignKey(TipoPrancha)
     altura = models.CharField(max_length=10, null=False)
     litragem = models.CharField(max_length=10, null=False)
-
 
     def __str__(self):
         return self.descricao
 
-class Aluguel(models.Model):
-    cliente = models.CharField(max_length=100, null=False)
+
+class Aluguel(Servico):
     prancha = models.ManyToManyField(Prancha)
-    data_aluguel = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
-        return self.cliente
+        return self.cliente.nome
+
+
+class Aula(models.Model):
+    professor = models.ManyToManyField(Professor)
+    alunos = models.ManyToManyField(Aluno)
+    horario = models.DateTimeField()
+
+    def __str__(self):
+        data_hora = self.horario.__str__().split(' ')
+        data = data_hora[0].split('-')
+        hora = data_hora[1][:5]
+        aula = data[2] + '/' + data[1] + '/' + data[0] + ' ' + hora
+        return aula
+
+
+class AulaAvulsa(Servico):
+    aula = models.ForeignKey(Aula)
+
+    def __str__(self):
+        return self.cliente.nome + ' (' + self.data.__str__() + ')'
+
+
+class PacoteAula(Servico):
+    aulas_restantes = models.IntegerField()
+    aulas = models.ManyToManyField(Aula)
+
+    def __str__(self):
+        return self.cliente.nome
