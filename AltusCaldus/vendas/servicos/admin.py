e787c2fd2ff26ce615146583001aaa866de3144b
AndyVitoria <andre_davitoria@hotmail.com>
2017-06-14 08:29:42 -0300

    Renomeação das classes DetalheAula, DetalheAluguel e DetalheGuarderia para ItemAula, ItemAluguel e ItemGuarderia

--- AltusCaldus/vendas/servicos/admin.py
+++ AltusCaldus/vendas/servicos/admin.py
@@ -21,7 +21,7 @@ admin.site.register(Item)
 class GuarderiaDetailInline(admin.TabularInline):
     list_display = ['item']
     readonly_fields = ['get_subtotal']
-    model = DetalheGuarderia
+    model = ItemGuarderia
     extra = 0
 
 
@@ -41,7 +41,7 @@ admin.site.register(TipoPrancha)
 class AluguelDetailInline(admin.TabularInline):
     list_display = ['prancha']
     readonly_fields = ['get_subtotal']
-    model = DetalheAluguel
+    model = ItemAluguel
     extra = 0
 
 
@@ -61,7 +61,7 @@ admin.site.register(AulaMarcada)
 class AulaDetailInline(admin.TabularInline):
     list_display = ['aula', 'professor']
     readonly_fields = ['get_subtotal']
-    model = DetalheAula
+    model = ItemAula
     extra = 0
 
 
