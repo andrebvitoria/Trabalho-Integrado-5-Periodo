6885ced01bd41c4e07e169ba03e602fb950ece76
Ana Rúbia <anarubiarvicente@gmail.com>
2017-06-17 23:20:21 -0300

    Update wsgi.py

--- AltusCaldus/vendas/wsgi.py
+++ AltusCaldus/vendas/wsgi.py
@@ -1,10 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vendas.settings")
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
