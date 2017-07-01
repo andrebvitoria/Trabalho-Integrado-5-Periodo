74fd3dfb22b7659a6c3973d3a7af2df15ac0bac7
AndyVitoria <andre_davitoria@hotmail.com>
2017-07-01 18:00:42 -0300

    Correção dos Links das paginas html de servicos

--- AltusCaldus/servicos/urls.py
+++ AltusCaldus/servicos/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^$', views.index),
-    url(r'aula/', views.aula),
-    url(r'guarderia/', views.guarderia),
-    url(r'aluguel/', views.aluguel),
+    url(r'aula/', views.aula, name='aula'),
+    url(r'guarderia/', views.guarderia, name='guarderia'),
+    url(r'aluguel/', views.aluguel, name='aluguel'),
 ]
