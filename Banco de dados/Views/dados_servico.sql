92b5c8f25a21ccf02eccd6e89eb44b8da2d44eae
Juliana Yuri <julianakanezaki@gmail.com>
2017-07-06 14:15:01 -0300

    Update dados_servico.sql

--- Banco de dados/Views/dados_servico.sql
+++ Banco de dados/Views/dados_servico.sql
@@ -1,17 +1,12 @@
-CREATE VIEW dados_aluguel AS 
-SELECT A.data, A.valor, A.desconto, V.nome as vendedor, C.nome as aluno 
-FROM aluguel AS A
-INNER JOIN vendedor AS V ON A.id_vendedor = V.id_vendedor
-INNER JOIN aluno AS C ON A.id_vendedor = C.id_aluno;
+CREATE VIEW dados_servico AS
+SELECT A.data, A.desconto, V.nome as vendedor, C.nome as aluno 
+FROM servico AS A
+INNER JOIN vendedor AS V ON A.id_vendedor = V.id_pessoa
+INNER JOIN aluno AS C ON A.id_aluno = C.id_pessoa;
 
-CREATE VIEW dados_aula AS 
-SELECT A.data, A.valor, A.desconto, V.nome as vendedor, C.nome as aluno 
-FROM aula AS A
-INNER JOIN vendedor AS V ON A.id_vendedor = V.id_vendedor
-INNER JOIN aluno AS C ON A.id_vendedor = C.id_aluno;
-
-CREATE VIEW dados_guarderia AS 
-SELECT A.data, A.vencimento, A.valor, A.desconto, V.nome as vendedor, C.nome as aluno 
+CREATE VIEW dados_guarderia AS
+SELECT A.data, A.desconto, A.vencimento, V.nome as vendedor, C.nome as aluno 
 FROM guarderia AS A
-INNER JOIN vendedor AS V ON A.id_vendedor = V.id_vendedor
-INNER JOIN aluno AS C ON A.id_vendedor = C.id_aluno;
+INNER JOIN vendedor AS V ON A.id_vendedor = V.id_pessoa
+INNER JOIN aluno AS C ON A.id_aluno = C.id_pessoa;
+
